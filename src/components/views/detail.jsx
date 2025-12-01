5450fa4677e9dcb4fd33ce1913b8122bcfd016af
Sergio <sergiotestera666@gmail.com>
2025-12-01 17:24:23 +0100

    Ultimos cambios a la ventana 'Detalles'

--- src/components/views/detail.jsx
+++ src/components/views/detail.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getPokemonById } from '../../pokeApi/pokeAxios.js';
 import Navbar from '../navbar/navbar'
-import Carta from '../card/card';
 import "../../styles/detail.css"
 
 const Details = () => {
@@ -27,73 +26,58 @@ const Details = () => {
                     text="Detalles"
                 />
                 
-                <div className="pokemon-detail-card mt-5">
+                <div className="mt-4">
                     <div>
-                        <div>
-                            <h4>#{pokemon.id.toString().padStart(3, '0')}</h4>
-                            <h1>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
-                        </div>
-                        <div className="pokemon-image-container">
-                            <div className="pokemon-image-wrapper">
-                                <img 
-                                    src={pokemon.sprites.front_default} 
-                                    className="pokemon-image"
-                                />
-                            </div>
-                        </div>
-                        <div className="pokemon-types">
-                            {pokemon.types.map((type) => (
-                                <h1 className={`type-badge type-${type.type.name}`}>
-                                    {type.type.name.toUpperCase()}
-                                </h1>
-                            ))}
+                        <h3>#{pokemon.id.toString().padStart(3, '0')} - {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>      {/*PARA QUE LOS NUMEROS SE VEAN 007 por ejemplo*/}
+             
+                    </div>
+                    <div className="pokemon-image-container">
+                        <div className="pokemon-image-wrapper">
+                            <img 
+                                src={pokemon.sprites.front_default} 
+                                className="pokemon-image"
+                            />
                         </div>
                     </div>
+                    <div className="pokemon-types">
+                        {pokemon.types.map((type) => (
+                            <h1 className={`type-badge type-${type.type.name}`}>
+                                {type.type.name.toUpperCase()}
+                            </h1>
+                        ))}
+                    </div>
+                </div>
 
-                    <div className="stats-column">
-                        <div className="stat-item">
-                            <div className="stat-label">
-                                HP
-                            </div>
-                            <div>
-                                <span className="stat-value">{pokemon.stats[0].base_stat}</span>
-                                <div style={{ width: `${(pokemon.stats[0].base_stat / 255) * 100}%` }}></div>
-                            </div>
+                <div className="stats-column">
+                    <div className="stat-item">
+                        <div className="stat-label">
+                            HP
                         </div>
-                        <div className="stat-item">
-                            <div className="stat-label">
-                                ATK
-                            </div>
-                            <div>
-                                <span className="stat-value">{pokemon.stats[1].base_stat}</span>
-                                <div style={{ width: `${(pokemon.stats[1].base_stat / 255) * 100}%` }}></div>
-                            </div>
+                        <span className="stat-value">{pokemon.stats[0].base_stat}</span>
+                    </div>
+                    <div className="stat-item">
+                        <div className="stat-label">
+                            ATK
                         </div>
-                        <div className="stat-item">
-                            <div className="stat-label">
-                                DEF
-                            </div>
-                            <div>
-                                <span className="stat-value">{pokemon.stats[2].base_stat}</span>
-                                <div style={{ width: `${(pokemon.stats[2].base_stat / 255) * 100}%` }}></div>
-                            </div>
+                        <span className="stat-value">{pokemon.stats[1].base_stat}</span>
+                    </div>
+                    <div className="stat-item">
+                        <div className="stat-label">
+                            DEF
                         </div>
-                        <div className="stat-item">
-                            <div className="stat-label">
-                                Altura
-                            </div>
-                            <div>
-                                <span className="stat-value">{pokemon.height / 10} m</span>
-                            </div>
+                        <span className="stat-value">{pokemon.stats[2].base_stat}</span>
+                    </div>
+                    <div className="stat-item">
+                        <div className="stat-label">
+                            Altura
                         </div>
-                        <div className="stat-item">
-                            <div className="stat-label">
-                                Peso
-                            </div>
-                            <div>
-                                <span className="stat-value">{pokemon.weight / 10} kg</span>
-                            </div>
+                        <span className="stat-value">{pokemon.height / 10} m</span>
+                    </div>
+                    <div className="stat-item">
+                        <div className="stat-label">
+                            Peso
                         </div>
+                        <span className="stat-value">{pokemon.weight / 10} kg</span>
                     </div>
                 </div>
             </div>
